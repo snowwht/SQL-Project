ff715eaf3a7be28506f167e390aee71a0d10e603
snowwhite <snowwht02@gmail.com>
2023-09-24 23:55:18 +0700

    first commit

---
+++ SQL Server Integration Services/ProfileMHS.sql
@@ -0,0 +1,61 @@
+create database ACT_SSIS_1
+
+use ACT_SSIS_1
+
+Create table ProfileMHS (
+ID int primary key,
+Name Varchar(100),
+MotherName varchar(100),
+FatherName varchar(100)
+)
+
+insert into ProfileMHS select id = 001,name ='Willie Turner',MotherName='Angela Estes',FatherName='James Schmidt'
+insert into ProfileMHS select id = 002,name ='Timothy Martinez',MotherName='Shirley Andrews',FatherName='Matthew Santiago'
+insert into ProfileMHS select id = 003,name ='Douglas Hoffman',MotherName='Shelley Reyes',FatherName='Vincent Mann'
+insert into ProfileMHS select id = 004,name ='Raymond Edwards',MotherName='Courtney Hall',FatherName='Michael Bentley'
+insert into ProfileMHS select id = 005,name ='Melissa Mcdonald',MotherName='Robin Aguilar',FatherName='David West'
+insert into ProfileMHS select id = 006,name ='Mario Adams',MotherName='April Owens',FatherName='Robert Watts'
+insert into ProfileMHS select id = 007,name ='Kari Jordan',MotherName='Mrs. Jessica Perez',FatherName='Jeremy Simpson'
+insert into ProfileMHS select id = 008,name ='Scott Ellison',MotherName='Catherine Cobb',FatherName='John West'
+insert into ProfileMHS select id = 009,name ='Jason Clayton',MotherName='Kristin Golden',FatherName='George Harris'
+insert into ProfileMHS select id = 010,name ='Alice Jacobs',MotherName='Amanda Meza',FatherName='Joshua Brown'
+insert into ProfileMHS select id = 011,name ='Jason Davis',MotherName='Elizabeth Salazar',FatherName='Matthew Bennett'
+insert into ProfileMHS select id = 012,name ='Samuel Griffin',MotherName='Stephanie Ritter',FatherName='Fernando Adams'
+insert into ProfileMHS select id = 013,name ='Brian Porter',MotherName='Sherry Schroeder',FatherName='Mr. Brian Bowman'
+insert into ProfileMHS select id = 014,name ='Matthew Nichols',MotherName='Gabriela Higgins',FatherName='Frank Carpenter'
+insert into ProfileMHS select id = 015,name ='Kaitlyn Davis',MotherName='Loretta Simmons',FatherName='Scott Robinson'
+insert into ProfileMHS select id = 016,name ='Cameron Gibson',MotherName='Jill Hooper',FatherName='Adam Bernard'
+insert into ProfileMHS select id = 017,name ='Jonathan Hernandez',MotherName='Amy Meyers',FatherName='Mr. Douglas Knight'
+insert into ProfileMHS select id = 018,name ='Justin Randolph',MotherName='Tanya Fowler',FatherName='Allen Phillips'
+insert into ProfileMHS select id = 019,name ='Kevin Valentine',MotherName='Ms. Alicia Young',FatherName='Mark Alvarez'
+insert into ProfileMHS select id = 020,name ='Carrie Frey',MotherName='Patricia Perez',FatherName='Jose Harris'
+insert into ProfileMHS select id = 021,name ='Elizabeth Hernandez',MotherName='Teresa Smith',FatherName='Gary Villegas'
+insert into ProfileMHS select id = 022,name ='Eric Ball',MotherName='Kim Williams',FatherName='George Burns'
+insert into ProfileMHS select id = 023,name ='Lisa Bailey',MotherName='Veronica Hartman',FatherName='Jesus Benjamin'
+insert into ProfileMHS select id = 024,name ='Tonya Taylor',MotherName='Sharon Taylor',FatherName='Richard Lester'
+insert into ProfileMHS select id = 025,name ='Thomas Schmidt',MotherName='Ashley Vasquez',FatherName='Steve Powell'
+insert into ProfileMHS select id = 026,name ='Stephanie Mclaughlin',MotherName='Kathleen Hale',FatherName='Drew Williams'
+insert into ProfileMHS select id = 027,name ='Kelli Bautista',MotherName='Taylor Cooper',FatherName='Shawn Barton'
+insert into ProfileMHS select id = 028,name ='William Hawkins',MotherName='Jessica Fuller',FatherName='Joseph Johnson'
+insert into ProfileMHS select id = 029,name ='Anthony Mendoza',MotherName='Christina Strickland',FatherName='Michael Perry'
+insert into ProfileMHS select id = 030,name ='David Savage',MotherName='Anita Mendez',FatherName='William Ramirez'
+insert into ProfileMHS select id = 031,name ='Dakota Baker',MotherName='Barbara Young',FatherName='Marc Moore'
+insert into ProfileMHS select id = 032,name ='Joseph King',MotherName='Rachel Perkins',FatherName='David Dickerson'
+insert into ProfileMHS select id = 033,name ='Joseph Morales',MotherName='Angela Jennings',FatherName='Nicholas Smith'
+insert into ProfileMHS select id = 034,name ='Jordan Williams',MotherName='Deborah Herrera',FatherName='David Cook'
+insert into ProfileMHS select id = 035,name ='Steven Gill',MotherName='Tammie Phillips',FatherName='Kyle Rodriguez'
+insert into ProfileMHS select id = 036,name ='Roberto Tucker',MotherName='Jennifer Lawson',FatherName='Brandon Cooper'
+insert into ProfileMHS select id = 037,name ='Kara Wright',MotherName='Christine Johns',FatherName='Eric Beasley'
+insert into ProfileMHS select id = 038,name ='Frederick Clark',MotherName='Christine Silva',FatherName='David Moore'
+insert into ProfileMHS select id = 039,name ='Michael Dawson',MotherName='Krystal Kirby',FatherName='James Chang'
+insert into ProfileMHS select id = 040,name ='Brian Santiago',MotherName='Rebecca Brooks',FatherName='Richard Wood'
+insert into ProfileMHS select id = 041,name ='Jenna Garcia',MotherName='Monique Peterson PhD',FatherName='Jason Nelson'
+insert into ProfileMHS select id = 042,name ='Heather Robertson',MotherName='Christine Martin',FatherName='Timothy Clark'
+insert into ProfileMHS select id = 043,name ='Mike Sharp',MotherName='Olivia Cherry',FatherName='Chris Howard'
+insert into ProfileMHS select id = 044,name ='Jacob Harding',MotherName='Andrea Harris',FatherName='Steven Sims'
+insert into ProfileMHS select id = 045,name ='Melinda Pratt',MotherName='Jennifer Riley',FatherName='Kevin Wilson'
+insert into ProfileMHS select id = 046,name ='David Wilson',MotherName='Maria Wilson',FatherName='Edward King'
+insert into ProfileMHS select id = 047,name ='Reginald Ortega',MotherName='Stephanie Phillips',FatherName='Benjamin Grimes'
+insert into ProfileMHS select id = 048,name ='Anthony Harris',MotherName='Jacqueline Campbell',FatherName='Timothy Yates'
+insert into ProfileMHS select id = 049,name ='John Gonzalez',MotherName='Kathryn Bryant',FatherName='Ryan Barnes'
+insert into ProfileMHS select id = 050,name ='Jennifer Campbell',MotherName='Nicole Smith',FatherName='Nathan Brown'
